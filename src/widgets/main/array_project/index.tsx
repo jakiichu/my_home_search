e842e3516c30610d67cfa8bc7ba93fe01cf6518b
jakiichu4 <zellda444@gmail.com>
2024-07-17 12:36:44 +0700

    add project

--- src/widgets/main/array_project/index.tsx
+++ src/widgets/main/array_project/index.tsx
@@ -1,4 +1,4 @@
-import {mdiKeyboard} from "@mdi/js";
+import {mdiAccount, mdiGitlab, mdiKeyboard} from "@mdi/js";
 import {ReactNode} from "react";
 import {IProjectsArray} from "@/shared/interface/project";
 import ProjectItem from "@/features/main/project";
@@ -11,7 +11,18 @@ const ArrayProject = (): ReactNode => {
             title: 'monkeytype',
             url: 'https://monkeytype.com/',
             path: mdiKeyboard
-        }
+        },
+        {
+            keyCombination: '2',
+            title: 'gitlab.ipst',
+            url: 'https://gitlab.ipst-dev.com/',
+            path: mdiGitlab
+        }, {
+            keyCombination: '3',
+            title: 'ipst.stage',
+            url: 'https://ipst-platform.stage.ipst-dev.com/report',
+            path: mdiAccount
+        },
     ]
     return (
         <div className={`grid grid-cols-4 gap-6 w-full max-w-[1200px] mx-auto`}>
